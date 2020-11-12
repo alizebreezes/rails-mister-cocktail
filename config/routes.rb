533c2f732011d3235ecdfa9632ab37ce3ed53508
Alize Lee <alizebreezes@gmail.com>
2020-11-12 20:33:34 +0100

    finished cocktail views and models and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'cocktails', to: 'cocktails#index'
+
+  get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
+  post 'cocktails', to: 'cocktails#create'
+
+  get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+uri = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+# this is a string
+drinks_info = open(uri).read
+# result_hash is an array with hash
+result_hash = JSON.parse(drinks_info)
+# the result_hash["drinks"].class is an array again
+
+ingredients_array = result_hash["drinks"].sample(15)
+
+ingredients_array.each do |hash|
+  Ingredient.create(name: hash['strIngredient1'])
+end
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
