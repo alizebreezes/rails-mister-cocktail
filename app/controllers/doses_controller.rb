5d9fde2eded47dfdaaee1ebffad3e691ab9f578a
Alize Lee <alizebreezes@gmail.com>
2020-11-12 21:18:56 +0100

    have to work from dose controller

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,23 @@
+class DosesController < ApplicationController
+  def index
+    @doses = Dose.all
+  end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+
+    if @dose.save
+      redirect_to @dose
+    else
+      render :new
+    end
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+end
